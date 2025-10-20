5fbf71ae71d483cedadd982c31d39bf4085fe4a8
DarkestRainn <mr.dolzhikov@inbox.ru>
2025-10-20 16:04:48 +0300

    Some changes in script

--- oh-my-zsh_and_more.sh
+++ oh-my-zsh_and_more.sh
@@ -1,13 +1,14 @@
 #!/bin/bash
 echo "Installing zsh..."
 apt update
-apt install zsh
+apt install zsh curl git
 Echo "Installing oh-my-zsh..."
 y | sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
 echo "Installing plugins..."
 git clone --depth=1 https://github.com/romkatv/powerlevel10k.git ${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/themes/powerlevel10k
 git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
 git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
+chsh -s $(which zsh)
 
 echo "Changing config .zshrc..."
 sed -i '/ZSH_THEME/s/robbyrussell/powerlevel10k\/powerlevel10k/' .zshrc
